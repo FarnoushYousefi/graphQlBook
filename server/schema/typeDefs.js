846aa65c0c4b243f894df23d0f88aae416f0f371
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-09-13 17:55:32 -0400

    remove the develop folder

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID
+    email: String
+    username: String!
+    savedBooks: [Book]
+    bookCount: Int
+  }
+  input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addBook(BookData: bookInput!): User
+    deleteBook(bookId: String!): User
+  }
+`;
+module.exports = typeDefs;
